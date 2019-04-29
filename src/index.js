ad02412cc4aadc537f527c8c84ed9e1f952ee3c9
skobraf <skobraf@gmail.com>
2019-04-29 16:25:56 +0100

    linked react with Apollo client

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
 
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
@@ -31,7 +32,9 @@ const client = new ApolloClient({
 
 
 ReactDOM.render(
-  <App />,
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
