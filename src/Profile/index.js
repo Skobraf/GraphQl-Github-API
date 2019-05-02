bda365a95ac19379d24dc66d79ed6caf268cbc92
skobraf <skobraf@gmail.com>
2019-05-02 11:54:07 +0100

    initiate repo to display users

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -3,6 +3,7 @@ import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 import Loading from '../Loading';
 import ErrorMessage from '../Error';
+import Repos from '../Repos';
 
 const GET_CURRENT_USER = gql`
  query ($query:String!, $cursor: String) {
@@ -34,29 +35,21 @@ const GET_CURRENT_USER = gql`
 }
 `;
 class Profile extends React.Component {
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll);
-  }
 
-  componentWillUnmount() {
-    window.removeEventListener('scroll', this.handleScroll);
-  }
-  handleScroll = () => {
-    window.onscroll = () => {
-      // Checks that the page has scrolled to the bottom
-      if (
-        window.innerHeight + document.documentElement.scrollTop ===
-        document.documentElement.offsetHeight
-      ) {
-        this.loadUsers();
-      }
-    };
-  }
   render () {
+    const date = new moment(new Date()).substract(1, 'months');
+    const dateFormat = date.format("YYYY-MM-DD");
+    const query = `is:public archived:false sort:stars-desc created:>${dateFormat}`;
     return (
-      <Query query={GET_CURRENT_USER}>
-      {({ data, loading, error }) => {
-        const { search } = data;
+      <Query
+      query={GET_CURRENT_USER}
+      notifyOnNetworkStatusChange={true}
+      variables={{
+        query
+      }}
+      >
+      {({ data, loading, error, fetchMore }) => {
+        const { search } = this.data;
           if (error) {
               return <ErrorMessage error={error} />
           }
@@ -65,9 +58,31 @@ class Profile extends React.Component {
             return <Loading />;
         }
         return (
-          <div>
-              {console.log(search)}
-          </div>
+          <Repos
+          loading={loading}
+          entries={search}
+          onLoadMore={() =>
+            fetchMore({
+              variables: {
+                query,
+                cursor: search.pageInfo.endCursor
+              },
+              updateQuery: (prevResult, { fetchMoreResult }) => {
+                const newEdges = fetchMoreResult.search.edges;
+                const pageInfo = fetchMoreResult.search.pageInfo;
+                return newEdges.length
+                  ? {
+                      search: {
+                        __typename: prevResult.search.__typename,
+                        edges: [...prevResult.search.edges, ...newEdges],
+                        pageInfo
+                      }
+                    }
+                  : prevResult;
+              }
+            })
+          }
+        />
         );
       }}
     </Query>
